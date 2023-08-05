8930c1a472e0ea61979bfb807fd1c6fd3b25c57e
sabamgeliashvili <sba69717@gmail.com>
2023-08-05 15:56:28 +0400

    new commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// burger menu 
+
+const hamburger = document.querySelector('.hamburger');
+const navmenu = document.querySelector('.nav-menu');
+hamburger.addEventListener('click', () => {
+   hamburger.classList.toggle ("active");
+   navmenu.classList.toggle ("active");
+})
+
+
+document.getElementById('submitbtn').addEventListener('click', function()
+ { alert('your message was sent successfully') 
+})
